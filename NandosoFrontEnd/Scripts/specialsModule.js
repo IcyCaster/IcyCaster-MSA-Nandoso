208e29e6d8c71f35d139224533e67d34219db920
IcyCaster <Chester.booker.nz@gmail.com>
2015-11-24 03:48:54 +1300

    Figured out missing css reference. bootstrap.min.cs.

---
+++ NandosoFrontEnd/Scripts/specialsModule.js
@@ -0,0 +1,19 @@
+﻿// Module pattern, for promoting encapsulation.
+var SpecialsModule = (function () {
+
+    // Private things here
+
+    // Return things here
+    return {
+        getSpecials: function (callback) {
+
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url
+            })
+        }
+
+    };
+
+}());
