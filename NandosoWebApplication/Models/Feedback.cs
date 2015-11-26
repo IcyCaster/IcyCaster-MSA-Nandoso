520041faf247fd829b1eb8c19dba7dab93e2c189
IcyCaster <Chester.booker.nz@gmail.com>
2015-11-27 03:22:37 +1300

    Tweaks made to Feedback.

--- NandosoWebApplication/Models/Feedback.cs
+++ NandosoWebApplication/Models/Feedback.cs
@@ -13,9 +13,8 @@ namespace NandosoWebApplication.Models
         public string Comment { get; set; }
         public string UserImageURL { get; set; }
         public string UsernameOrTitle { get; set; }
-
-        [JsonIgnore]
         public int ParentID { get; set; }
+
         [JsonIgnore]
         public int UserID { get; set; }
     }
